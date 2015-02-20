613f58a5b4aad8db4c4e436fbe0ed004bba32d41
ktrinca <yraspo@gmail.com>
2015-02-19 21:57:09 -0300

    Add Contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219235126) do
+ActiveRecord::Schema.define(version: 20150220005455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150219235126) do
   create_table "cities", force: true do |t|
     t.string   "city"
     t.integer  "province_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "last_name"
+    t.integer  "city_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
