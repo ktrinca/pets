9ea2943518c0efe4767b9c5dddebbd43b6603325
ktrinca <yraspo@gmail.com>
2015-02-20 17:58:13 -0300

    Add comments Add relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new 
+    @categories = Categories.all
+  end
+
+  def create 
+  end	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :contact
+  has_many   :comments
 end
