1ad341c763e69e1d628c06315880e15a33302cc2
ktrinca <yraspo@gmail.com>
2015-03-03 17:15:25 -0300

    Modify test to categories pages

--- test/integration/categories_pages_test.rb
+++ test/integration/categories_pages_test.rb
@@ -1,12 +1,8 @@
 require "test_helper"
 
 class CategoriesPagesTest < ActionDispatch::IntegrationTest
-  before do
-    category =  create(:category, posts: [create(:post)])
-    visit category_path(category)  
-  end	
-  
   test "should list all available posts" do
-    assert_response :success
+    category =  create(:category, posts: [create(:post)])
+    assert_equal '/categories/1', category_path(category)
   end
 end
