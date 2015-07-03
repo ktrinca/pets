02189b8f2d073afa2dc5fcd90cc7926150d53a1b
ktrinca <yraspo@gmail.com>
2015-07-03 10:52:54 -0300

    Change style show:  Add style  Add slide to image  Delete div with contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ActiveRecord::Base
   has_many :posts
   validates :phone, :name, :last_name,  presence: true
+
+  def full_name
+   "#{self.name.capitalize} #{self.last_name.capitalize} "
+  end	
 end
