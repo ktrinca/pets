05fec0a0343730e8351497eac160745baecc45ac
ktrinca <yraspo@gmail.com>
2015-07-29 09:27:24 -0300

    Add method delete_parents

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
+  before_destroy :delete_parents	
   belongs_to :user
   belongs_to :post
   acts_as_tree order: 'created_at DESC'
+
+  private
+    def delete_parents
+      self.class.delete_all "parent_id = #{id}"
+    end
 end
